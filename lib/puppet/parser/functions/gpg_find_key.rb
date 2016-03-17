08fa552632fbdb6b93284a20c6bd17639cd770ef
Jan Vansteenkiste <jan@vstone.eu>
2016-03-17 13:56:13 +0100

    Fix tabs (rubocop)

--- lib/puppet/parser/functions/gpg_find_key.rb
+++ lib/puppet/parser/functions/gpg_find_key.rb
@@ -1,30 +1,30 @@
 module Puppet::Parser::Functions
 
-	newfunction(:gpg_find_key, :type => :rvalue) do |args|
-		keys = args.shift || {}
-		criteria = args.shift || {}
+  newfunction(:gpg_find_key, :type => :rvalue) do |args|
+    keys = args.shift || {}
+    criteria = args.shift || {}
 
-		if keys == ""
-			keys = {}
-		end
+    if keys == ""
+      keys = {}
+    end
 
-		if criteria == ""
-			criteria = {}
-		end
+    if criteria == ""
+      criteria = {}
+    end
 
-		matching_keys = keys.keep_if { |key, value|
-			keep = true
-			criteria.keys.each { |c|
-				keep &&= criteria[c] == value[c]
-			}
-			keep
-		}
+    matching_keys = keys.keep_if { |key, value|
+      keep = true
+      criteria.keys.each { |c|
+        keep &&= criteria[c] == value[c]
+      }
+      keep
+    }
 
-		if matching_keys.length == 0
-			nil
-		else
-			matching_keys[ matching_keys.keys[0] ]
-		end
-	end
+    if matching_keys.length == 0
+      nil
+    else
+      matching_keys[ matching_keys.keys[0] ]
+    end
+  end
 
 end
