601cdb1a019b7667f5d49822bc932eb152bc043f
Jeroen Hooyberghs <jeroen@hooyberghs.org>
2019-05-03 16:10:12 +0200

    Support for not managing the /etc/gpg_keys dir from the module (allowing for example symlinks)

--- manifests/gpg.pp
+++ manifests/gpg.pp
@@ -1,10 +1,12 @@
-class keypair::gpg {
-
-  file { '/etc/gpg_keys':
-    ensure => directory,
-    mode   => '0755',
-    owner  => 'root',
-    group  => 'root',
+class keypair::gpg (
+  Boolean $manage_dir = true,
+) {
+  if $manage_dir {
+    file { '/etc/gpg_keys':
+      ensure => directory,
+      mode   => '0755',
+      owner  => 'root',
+      group  => 'root',
+    }
   }
-
 }
