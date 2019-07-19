917597182199c12d0201770c9f40a08c9baa406a
Jan Vansteenkiste <jan@vstone.eu>
2019-07-19 09:00:42 +0200

    Migrate x509_generate_key to modern ruby functions api

--- spec/functions/x509_generate_key_spec.rb
+++ spec/functions/x509_generate_key_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 
 describe 'x509_generate_key' do
-  it 'does exist' do
-    expect(Puppet::Parser::Functions.function(:x509_generate_key)).to eq('function_x509_generate_key')
+  context 'handle parameters' do
+    it do
+      is_expected.to run.with_params('CN' => 'www.example.org', 'key_length' => 512)
+    end
+    it do
+      is_expected.to run.with_params(512, 'www.example.org')
+    end
+    it do
+      is_expected.to run.with_params('www.example.org')
+    end
+    it do
+      is_expected.to run.with_params(512)
+    end
+    it do
+      is_expected.to run.with_params
+    end
   end
 
   context do
     let(:out) do
-      scope.function_x509_generate_key(['CN' => 'www.example.org', 'key_length' => 512])
+      subject.execute('CN' => 'www.example.org', 'key_length' => 512)
     end
 
     it 'does return attributes' do
